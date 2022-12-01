4f497da340ae535d41904a9c77a41cd562ce4712
Ingvar <ingvarolsen57@hotmail.com>
2022-12-01 22:53:35 +0000

    sockettesting

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from website import create_app, create_database
+from sanic import Sanic
+from sanic.response import html
+
+import socketio
 
 app = create_app()
 #create_database(app)
+
+
+sio = socketio.AsyncServer(async_mode='sanic')
+app = Sanic()
+sio.attach(app)
+
+
+@app.route('/socketTest')
+def index(request):
+    with open('latency.html') as f:
+        return html(f.read())
+
+
+@sio.event
+async def ping_from_client(sid):
+    await sio.emit('pong_from_server', room=sid)
+
+app.static('/static', './static')
 
 
 if __name__ == '__main__':
