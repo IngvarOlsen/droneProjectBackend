886117e016af292e732936b2658832966bd15111
Ingvar <ingvarolsen57@hotmail.com>
2023-01-10 03:28:42 +0000

    SSL selfsigned add

--- main.py
+++ main.py
@@ -30,4 +30,5 @@ def download(filename):
     )
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    #app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, ssl_context=('cert.pem', 'certpriv_key.pem')) # For SSL
